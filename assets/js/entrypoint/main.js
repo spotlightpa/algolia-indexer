27c6d79627d5c3261bbf5db65fa8140c683a65bc
Carl Johnson <me@carlmjohnson.net>
2020-10-08 14:53:59 -0400

    JS: Redirect admin users

--- assets/js/entrypoint/main.js
+++ assets/js/entrypoint/main.js
@@ -5,3 +5,10 @@ import searchPeople from "../utils/search-people.js";
 window.spl = Object.assign({}, window.spl, {
   searchPeople,
 });
+
+// Redirect admin emails to admin
+const routes = /(confirmation|invite|recovery|email_change)_token=([^&]+)/g;
+
+if (window.location.hash.match(routes) ) {
+  window.location.replace(window.location.origin+"/admin/"+window.location.hash);
+}
