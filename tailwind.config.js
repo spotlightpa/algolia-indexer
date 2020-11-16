99a371cff4efbe2ed044e3720c78d17130c367e1
Carl Johnson <me@carlmjohnson.net>
2020-11-16 12:20:14 -0500

    Tailwind config: Format

--- tailwind.config.js
+++ tailwind.config.js
@@ -71,7 +71,7 @@ module.exports = {
     },
   },
   variants: {
-    backgroundColor: ['responsive', 'odd', 'hover', 'focus'],
+    backgroundColor: ["responsive", "odd", "hover", "focus"],
     boxShadow: ["responsive", "hover", "focus", "active", "group-hover"],
   },
   plugins: [],
